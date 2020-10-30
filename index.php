89fce8c3983ce4dc146b147796aeb0475fd9fc5d
it63070217 <73646540+it63070217@users.noreply.github.com>
2020-10-30 17:36:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-  <?php
-  echo "<h2>IT@KMITL</h2>";
-  echo "Hello IT<br>";
-  ?>
+ <html>
+  <head>
+    </title>Hello world</title>
+  </head>
+  <body>
+    <h1>My First Heading</h1>
+    <p>My First Paragraph</p>
+    <img src="cat.jpg">
+    <img src="cat1.jpg">
+    <br>
+    <a href="https://www.dek-d.com/">This is a Link</a>
+
+    <h2>The unordered list</h2>
+    <ul>
+      <li>นางสาวโชติกา ตาวงค์</li>
+      <li>เกด</li>
+    <ul>
+  
+  </body>
+</html>
