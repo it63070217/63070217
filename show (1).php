dc7c0c319ecd9dcd7c7b7c1d772ae64c1950636e
it63070217 <73646540+it63070217@users.noreply.github.com>
2020-11-13 17:34:20 +0700

    Update show (1).php

--- show (1).php
+++ show (1).php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'it63070217.mysql.database.azure.comm', 'it63070217@it63070217', 'it-63070217', 'ITFLab', 3306);
+mysqli_real_connect($conn, 'it63070217.mysql.database.azure.com', 'it63070217@it63070217', 'it-63070217', 'ITFLab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
