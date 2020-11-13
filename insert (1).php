9b3be3dd256b88f4db791419448c1045825f4d11
it63070217 <73646540+it63070217@users.noreply.github.com>
2020-11-13 17:25:54 +0700

    Add files via upload

---
+++ insert (1).php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'it63070217.mysql.database.azure.com', 'it63070217@it63070217', 'it-63070217', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+
+$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+mysqli_close($conn);
+?>
