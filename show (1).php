76d5b3c04d3ef45dafa3ff1bf1ddfa1f6a21ec0a
it63070217 <73646540+it63070217@users.noreply.github.com>
2020-11-15 18:47:03 +0700

    Update show (1).php

--- show (1).php
+++ show (1).php
@@ -16,7 +16,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
+    <th width="150"> <div align="center">Action </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
@@ -25,8 +25,8 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td><a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a></td>
-    <td><a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a></td>
+    <td><a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a>
+    <a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a></td>
   </tr>
 <?php
 }
