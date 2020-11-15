8f5e20b49f75db3b2658e3a0d4d076fb7db25745
it63070217 <73646540+it63070217@users.noreply.github.com>
2020-11-15 19:21:53 +0700

    Add files via upload

---
+++ exercise.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title>ITF Lab</title>
+</head>
+<body>
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'it63070217.mysql.database.azure.com', 'it63070217@it63070217', 'it-63070217', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+<table width="600" border="1">
+  <tr>
+    <th width="100"> <div align="center">Name</div></th>
+    <th width="350"> <div align="center">Comment </div></th>
+    <th width="150"> <div align="center">Action </div></th>
+  </tr>
+<?php
+while($Result = mysqli_fetch_array($res))
+{
+?>
+  <tr>
+    <td><?php echo $Result['Name'];?></div></td>
+    <td><?php echo $Result['Comment'];?></td>
+    <td align="center"><a href="edit.php" role="button">ลบ</a>
+    <a href="delete.php" role="button">แก้ไข</a></td>
+  </tr>
+<?php
+}
+?>
+</table>
+<?php
+mysqli_close($conn);
+?>
+<a href="https://63070217.azurewebsites.net/form%20(2).html" role="button">เพิ่ม</a>
+</body>
+</html>
