dde69efe61b547f534a0c8e9de36d2a029a3fa2b
it63070217 <73646540+it63070217@users.noreply.github.com>
2020-11-19 17:33:16 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,7 +1,12 @@
 <?php
-require('db.php');
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'it63070217.mysql.database.azure.com', 'it63070217@it63070217', 'it-63070217', 'ITFLab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 $id=$_REQUEST['id'];
-$query = "DELETE FROM new_record WHERE id=$id"; 
+$query = "DELETE FROM guestbook WHERE id=$id"; 
 $result = mysqli_query($con,$query) or die ( mysqli_error());
-header("Location: view.php"); 
+header("Location: show.php"); 
 ?>
