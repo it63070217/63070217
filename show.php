1b5b4ba0033b76334b6200192db8731e42a1b66c
it63070217 <73646540+it63070217@users.noreply.github.com>
2020-11-19 17:19:53 +0700

    Update show.php

--- show.php
+++ show.php
@@ -28,7 +28,6 @@ while($Result = mysqli_fetch_array($res))
     <td><a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a>
     <a href="edit.php?id=<?php echo $row["id"]; ?>">Edit</a></td>
   </tr>
-  <type="input" id="commentBtn">
 <?php
 }
 ?>
