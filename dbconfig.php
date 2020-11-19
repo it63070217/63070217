6577cbba71cd26bd63171914c32e7d44d3c69d19
it63070217 <73646540+it63070217@users.noreply.github.com>
2020-11-19 20:56:46 +0700

    Add files via upload

---
+++ dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'host' => 'it63070217.mysql.database.azure.com',
+    'username' => 'it63070217@it63070217',
+    'password' => 'it-63070217',
+    'database' => 'ITFLab',
+);
+?>
