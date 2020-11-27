33a0ce658f8ef160d49b56f065cf3648cc702d6d
it63070217 <73646540+it63070217@users.noreply.github.com>
2020-11-27 16:51:57 +0700

    Update test_insert.php

--- test_insert.php
+++ test_insert.php
@@ -1,18 +1,20 @@
 <?php
         $conn = mysqli_init();
-            mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070234', 'SMSylb15', 'it63070234_film', 3306);
+            mysqli_real_connect($conn, 'it63070217.mysql.database.azure.com', 'it63070217@it63070217', 'it-63070217', 'test', 3306);
             if (mysqli_connect_errno($conn))
             {
                 die('Failed to connect to MySQL: '.mysqli_connect_error());
             }
 
-        $A = $_GET['A'];
-        $B = $_GET['B'];
-        $C = $A+$B;
-        $sql = "INSERT INTO abc (A , B , C) VALUES ('$A', '$B', '$C')";
+        $Id = $_GET['Id'];
+        $name = $_GET['name'];
+        $height = $_GET['height'];
+        $weight = $_GET['weight'];
+        $bmi = $weight/($height*$height)
+        $sql = "INSERT INTO Guestbook (Id , name , height, weight, bmi) VALUES ('$Id', '$name', '$height', '$weight', '$bmi')";
    
         if (mysqli_query($conn, $sql)) {
-    header("location:show.php");
+    header("location:test_show.php");
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
