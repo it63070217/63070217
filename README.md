5c05d6747b16ba11f2dd889a8ce63648ed5a7110
it63070217 <73646540+it63070217@users.noreply.github.com>
2020-11-27 15:17:15 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# filmza007eiei
--- insert.php
+++ insert.php
@@ -1,36 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ITF Database Lab</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
 <?php
-$dbconfig = include('dbconfig.php');
-$conn = mysqli_connect($dbconfig['host'], $dbconfig['username'], $dbconfig['password'], $dbconfig['database']);
-if (!$conn)
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
+        $conn = mysqli_init();
+            mysqli_real_connect($conn, 'ihost.it.kmitl.ac.th', 'it63070234', 'SMSylb15', 'it63070234_film', 3306);
+            if (mysqli_connect_errno($conn))
+            {
+                die('Failed to connect to MySQL: '.mysqli_connect_error());
+            }
 
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
-$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
-
-if (mysqli_query($conn, $sql)) {
-    echo '<div class="container">
-            <h3>Comment has been saved in to the database successfully.</h3>
-            <a role="button" class="btn btn-primary mt-3" href="guestbook.php">Home</a>
-         </div>';
-} else {
+        $A = $_GET['A'];
+        $B = $_GET['B'];
+        $C = $A+$B;
+        $sql = "INSERT INTO abc (A , B , C) VALUES ('$A', '$B', '$C')";
+   
+        if (mysqli_query($conn, $sql)) {
+    header("location:show.php");
+  } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+  }
+        
   
-mysqli_close($conn);
+        mysqli_close($conn);
+        
 ?>
-</body>
-</html>
